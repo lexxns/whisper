b4e8072391f54fa62ed01dcfe7f3e872cf289197
jordan.aspinall <jaspinall@caci.co.uk>
2021-08-31 16:32:27 +0100

    Pass in a Note Letter to write that note

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ fn main() {
     );
 
     for t in (0..44100).map(|x| x as f32 / 44100.0) {
-        let sample = (t * 440.0 * 2.0 * PI).sin();
+        let sample = (t * note_to_freq("A") * 2.0 * PI).sin();
         let amplitude = i16::MAX as f32;
         writer.write_sample((sample * amplitude) as i16).unwrap();
     }
@@ -40,4 +40,22 @@ fn main() {
     );
 
     writer.finalize().unwrap();
+}
+
+// calculate the frequency in Hz of a given note
+fn note_to_freq(note: &str) -> f32 {
+    let mut freq = 440.0;
+    for c in note.chars() {
+        match c {
+            'C' => freq *= 1.0,
+            'D' => freq *= 2.0,
+            'E' => freq *= 3.0,
+            'F' => freq *= 4.0,
+            'G' => freq *= 5.0,
+            'A' => freq *= 6.0,
+            'B' => freq *= 7.0,
+            _ => panic!("Invalid note"),
+        }
+    }
+    freq
 }
