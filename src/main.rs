a831801f5c43bdcf4035e6c0a9ec368c2a2dda5a
jordan.aspinall <jaspinall@caci.co.uk>
2021-08-31 17:14:44 +0100

    switch to using piano key frequencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::thread_rng;
+use rand::Rng;
 use std::f32::consts::PI;
 use std::i16;
 use std::path::Path;
@@ -28,10 +30,14 @@ fn main() {
         writer.duration() / spec.sample_rate
     );
 
-    for t in (0..44100).map(|x| x as f32 / 44100.0) {
-        let sample = (t * note_to_freq("A") * 2.0 * PI).sin();
-        let amplitude = i16::MAX as f32;
-        writer.write_sample((sample * amplitude) as i16).unwrap();
+    let amplitude = i16::MAX as f32;
+    for _ in 1..10 {
+        let freq = random_key();
+        let duration = 1.0 * spec.sample_rate as f32;
+        for t in (0..duration as u32).map(|x| x as f32 / spec.sample_rate as f32) {
+            let sample = (t * freq * 2.0 * PI).sin();
+            writer.write_sample((sample * amplitude) as i16).unwrap();
+        }
     }
 
     println!(
@@ -42,20 +48,19 @@ fn main() {
     writer.finalize().unwrap();
 }
 
-// calculate the frequency in Hz of a given note
-fn note_to_freq(note: &str) -> f32 {
-    let mut freq = 440.0;
-    for c in note.chars() {
-        match c {
-            'C' => freq *= 1.0,
-            'D' => freq *= 2.0,
-            'E' => freq *= 3.0,
-            'F' => freq *= 4.0,
-            'G' => freq *= 5.0,
-            'A' => freq *= 6.0,
-            'B' => freq *= 7.0,
-            _ => panic!("Invalid note"),
-        }
-    }
-    freq
+// returns a random key frequency (using frequency_of_key) between 28 and 64 (inclusive)
+fn random_key() -> f32 {
+    let mut rng = thread_rng();
+    let key = rng.gen_range(28..65);
+    frequency_of_key(key)
+}
+
+// calculate the frequency in Hz of a given key
+// key is a number from 1 to 88
+// which corresponds to the note A0 (key1) to C8 (key88)
+// example key 1 = 27.5, key 88 = 4186.009
+fn frequency_of_key(key: u8) -> f32 {
+    let freq = 440.0;
+    let freq_of_key = freq * 2.0_f32.powf((key as f32 - 49.0) / 12.0);
+    freq_of_key
 }
